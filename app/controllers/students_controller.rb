5afa45d5fa8363548039fd30109425187340c714
Zach <zachlandis91@gmail.com>
2023-07-20 11:18:58 -0600

    commit tree 49a51da2738069793ea0ecb5039db7e6034442a1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
-    render json: students
-  end
+def index
+  students = Student.all
+  render json: students
+end
+
+def show
+  student = Student.find(params[:id])
+  render json: student
+end
 
 end
--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe "Students", type: :request do
       ])
     end
 
-    ## BONUS: un-comment out the code below to run the bonus test
+    # # BONUS: un-comment out the code below to run the bonus test
 
     # context 'with query params' do
     #   it 'returns students whose first or last name matches the query' do
